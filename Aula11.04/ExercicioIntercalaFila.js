fb1cc7706d1dde3d83e636a1729585b3393d129e
MATEUS SILVA ARAUJO <1090482311007@fatec-srv.local>
2024-04-16 11:02:48 -0300

    correção do exercicio de pilha

--- Aula11.04/ExercicioIntercalaFila.js
+++ Aula11.04/ExercicioIntercalaFila.js
@@ -1,6 +1,6 @@
 let fila = [0, 1, 2 ,3]
 let fila2 = [4, 5, 6 ,7]
-let fila3 = intercalaFila(fila, fila2)
+let fila3 = intercalaElementosFila(fila, fila2)
 console.log(fila3)
 
 function intercalaFila(fila1, fila2){
@@ -12,4 +12,16 @@ function intercalaFila(fila1, fila2){
         fila2.push(popado)
     }
     return fila3
+}
+
+function intercalaElementosFila(fila1, fila2){
+    let fila3 = []
+    let tam = fila1.length;
+    while(tam>0){
+        fila3.push(fila1.shift())
+        fila3.push(fila2.shift())
+        tam--
+    }
+    return fila3
+
 }
---
+++ Aula11.04/ExercicioPilhaCorrecao.js
@@ -0,0 +1,45 @@
+/* function invertePalavra(palavra){
+    let i = 0;
+    let elemento = palavra[i];
+    let tam = palavra.length;
+    let tam2;
+    let pilha = []
+    let pilhaInvertida = []
+    while(tam>0){
+        while(palavra[i] != " " && tam>0){
+            console.log(palavra[i])
+            pilha.push(palavra[i])
+            i++
+        }
+        tam2 = pilha.length
+        while(tam2>0){
+            pilhaInvertida.push(pilha.pop())
+            tam2--
+        }
+        console.log(tam)
+    }
+    return pilhaInvertida
+} */
+
+function invertePalavra(palavra){
+    let i = 0;
+    let pilha = []
+    let letra, fraseInvertida;
+    while(i < palavra.length){
+        while(palavra[i] != " " && i<palavra.length){
+            pilha.push(palavra[i])
+            i++
+        }
+        while(pilha.length != 0){
+            letra = pilha.pop()
+            fraseInvertida += letra
+        }
+        fraseInvertida += " "
+    }
+    return fraseInvertida
+}
+
+
+let palavra = "Ta feito jaqueline ;)"
+let fraseInvertida = invertePalavra(palavra)
+console.log(fraseInvertida)
