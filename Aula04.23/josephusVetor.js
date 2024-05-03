da9bf41b00e1f6798e0fc6a0609112ec48cb90ef
Mateus Silva Araujo <mtsSAraujo@users.noreply.github.com>
2024-05-03 12:47:01 -0300

    josephus Vetor

--- Aula04.23/josephusVetor.js
+++ Aula04.23/josephusVetor.js
@@ -4,7 +4,37 @@ function createVetor(vetor, n){
     }
 }
 
+function josephusResolution(vetor, m){
+    let tamanho = vetor.length;
+    let andar = m -1;
+    let posicaoAtual = 0;
+    let saldo;
+    while(tamanho>1){
+        if(posicaoAtual + andar < tamanho){
+            posicaoAtual = posicaoAtual + andar
+        }
+        else{
+            if(posicaoAtual - tamanho >= 0){
+                saldo = posicaoAtual - tamanho
+                posicaoAtual = saldo
+            }
+            for(let i = 0; i < andar; i++){
+                if(posicaoAtual + 1 < tamanho){
+                    posicaoAtual++
+                }
+                else{
+                    posicaoAtual = 0
+                }
+            }
+        }
+        vetor.splice(posicaoAtual, 1)
+        tamanho--
+    }
+}
+
 let vetor = []
-let n = 4, m = 3;
+let n = 50, m = 36;
 createVetor(vetor, n)
+console.log(vetor)
+josephusResolution(vetor, m)
 console.log(vetor)
